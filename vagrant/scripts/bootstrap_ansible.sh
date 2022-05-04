4769a35c014a6e90d8481a9e524c59971746653b
David Barroso <david.barroso@udl.cat>
2022-05-04 13:55:11 +0200

    Refactoring of ansible roles

--- vagrant/scripts/bootstrap_ansible.sh
+++ vagrant/scripts/bootstrap_ansible.sh
@@ -3,7 +3,5 @@
 set -e
 
 echo "Installing Ansible..."
-apt-get install -y software-properties-common
-apt-add-repository ppa:ansible/ansible
 apt-get update
-apt-get install -y --force-yes ansible
+apt-get install -y ansible
